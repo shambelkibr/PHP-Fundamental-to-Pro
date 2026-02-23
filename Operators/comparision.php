efa8bebb93db5510c4a71159a2abdf7a03283410
Shambelkibr <shambel5110@gmail.com>
2026-02-24 00:31:59 +0300

    PhP basic fundametal operators

---
+++ Operators/comparision.php
@@ -0,0 +1,36 @@
+<?php
+$a = 10;
+$b = "10";
+
+echo "$a == $b : ";
+var_dump($a == $b);
+echo "<br>";
+
+echo "$a === $b : ";
+var_dump($a === $b);
+echo "<br>";
+
+echo "$a != $b : ";
+var_dump($a != $b);
+echo "<br>";
+
+echo "$a !== $b : ";
+var_dump($a !== $b);
+echo "<br>";
+
+echo "$a > 5 : ";
+var_dump($a > 5);
+echo "<br>";
+
+echo "$a < 20 : ";
+var_dump($a < 20);
+echo "<br>";
+
+echo "$a >= 10 : ";
+var_dump($a >= 10);
+echo "<br>";
+
+echo "$a <= 10 : ";
+var_dump($a <= 10);
+echo "<br>";
+?>
