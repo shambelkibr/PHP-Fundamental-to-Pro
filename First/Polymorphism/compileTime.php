350fae62c21934ccc666239ae5517a77a3496a8f
shambelkibr <shambel5110@gmail.com>
2026-03-23 12:54:18 +0300

    add polymorphism function

--- First/Polymorphism/compileTime.php
+++ First/Polymorphism/compileTime.php
@@ -1,6 +1,6 @@
 <?php
 
-class Calculator{
+class Calculator1{
 
         public function __call($name,$arguments){
             if($name=="multiply"){
@@ -18,11 +18,44 @@ class Calculator{
         }
 }
 
-$calc=new Calculator();
+$calc=new Calculator1();
 echo $calc->multiply(2,3);
 echo "</br>";
 echo $calc->multiply(2,3,4);
 echo "</br>";
 echo $calc->multiply(2,3,4,5);
+
+
+echo "</br>";
+
+class Calculator2 {
+    // Method with default parameters
+    public function add($a, $b = 0, $c = 10) {
+        return $a + $b + $c;    }
+}
+$calc = new Calculator2();
+// Calling the same method with different number of arguments
+echo $calc->add(5) . "<br>";    // Output: 15  (adds only one number)
+echo $calc->add(5, 10) . "<br>";    // Output: 25     (adds two numbers)
+echo $calc->add(5, 10, 15) . "<br>";// Output: 30     (adds three numbers)
+
+
+class Area{
+        public function area($length,$width=null){
+            if ($width==null){
+                return $length*$length;
+            }
+            else{
+                return $length*$width;
+            }
+
+        }
+}
+
+$area1=new Area();
+echo "Area of square: ".$area1->area(5);
+
+$area2=new Area();
+echo "</br>Area of rectangle: ".$area2->area(5,10);
 
 ?>
