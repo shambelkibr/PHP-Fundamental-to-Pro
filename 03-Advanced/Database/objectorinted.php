9e4197556635a738ce0d835fe82ed37f13a2d28a
shanbelkibr <shambel5110@gmail.com>
2026-04-21 13:24:08 +0300

    add advanced concept mysql data base connection

---
+++ 03-Advanced/Database/objectorinted.php
@@ -0,0 +1,15 @@
+<?php
+
+$hostname = "127.0.0.1";
+$username = "shanbelkibre";
+$password = "Sha2025db@";
+
+$dbc = new mysqli($hostname, $username, $password);
+
+if ($dbc->connect_error) {
+    die("connection is failed " . $dbc->connect_error);
+}
+
+echo "successfully connected";
+
+?>
